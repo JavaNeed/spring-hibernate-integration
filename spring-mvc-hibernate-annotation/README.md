1fd04b15ca0724423ade7eb7e8af60d07395ba6e
prateek <prateek.ashtikar512@gmail.com>
2016-03-30 09:05:48 +0530

    Spring MVC + MYSQL + Spring Tx + JPA + Hibernate

---
+++ spring-mvc-hibernate-annotation/README.md
@@ -0,0 +1,8 @@
+spring-mvc-hibernate-annotation
+----------------------------------
+
+Application URL: http://localhost:8080/dpr-data/
+
+
+Reference URL:
+--------------
---
+++ spring-mvc-hibernate-annotation/src/main/java/com/spr/init/WebAppConfig.java
@@ -0,0 +1,103 @@
+package com.spr.init;
+
+import java.util.Properties;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.spr")
+@PropertySource("classpath:application.properties")
+@EnableJpaRepositories("com.spr.repository")
+public class WebAppConfig {
+
+	private static final String DATABASE_DRIVER = "db.driver";
+	private static final String DATABASE_PASSWORD = "db.password";
+	private static final String DATABASE_URL = "db.url";
+	private static final String DATABASE_USERNAME = "db.username";
+
+	private static final String HIBERNATE_DIALECT = "hibernate.dialect";
+	private static final String HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+	private static final String ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+
+	@Resource
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName(env.getRequiredProperty(DATABASE_DRIVER));
+		dataSource.setUrl(env.getRequiredProperty(DATABASE_URL));
+		dataSource.setUsername(env.getRequiredProperty(DATABASE_USERNAME));
+		dataSource.setPassword(env.getRequiredProperty(DATABASE_PASSWORD));
+
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+		entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(ENTITYMANAGER_PACKAGES_TO_SCAN));
+		
+		entityManagerFactoryBean.setJpaProperties(hibProperties());
+		
+		return entityManagerFactoryBean;
+	}
+
+	private Properties hibProperties() {
+		Properties properties = new Properties();
+		properties.put(HIBERNATE_DIALECT,	env.getRequiredProperty(HIBERNATE_DIALECT));
+		properties.put(HIBERNATE_SHOW_SQL, env.getRequiredProperty(HIBERNATE_SHOW_SQL));
+		return properties;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return transactionManager;
+	}
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+	
+	/**
+	 * This method get the message source
+	 * @return
+	 */
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename(env.getRequiredProperty("message.source.basename"));
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
+	}
+
+}
